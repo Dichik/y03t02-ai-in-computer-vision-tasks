c57cb4c269844f7645154d8cdf6504ba03013227
Dichik <omeluan.dima@gmail.com>
2023-04-10 14:18:03 +0300

    assignment 2: finished task 3

--- assignment-2/task3.py
+++ assignment-2/task3.py
@@ -1,39 +1,59 @@
 """
 Task #3
 
-Обробити вiдеофал, так щоб вiн видiляв пiшоходiв i, по можливостi, їхнi обличчя.
+Обробити вiдеофайл, так щоб вiн видiляв пiшоходiв i, по можливостi, їхнi обличчя.
 Файл можна взяти з youtube i вирiзати ролик тривалiстю не менше 30 секунд.
 
 """
 
 import cv2
-
-
-scaling_factor = 0.2
-import cv2
 import numpy as np
-
-hog = cv2.HOGDescriptor()
-hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-cv2.startWindowThread()
-
-cap = cv2.VideoCapture("../images/people_walk.mp4")
-while True:
-    ret, frame = cap.read()
-    # try:
-    #     frame = cv2.resize(frame, (800, 560))
-    # except:
-    #     continue
-    gray_filter = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-    boxes, weights = hog.detectMultiScale(frame, winStride=(8, 8))
-    boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
-    for (xa, ya, xb, yb) in boxes:
-        cv2.rectangle(frame, (xa, ya), (xb, yb), (0, 255, 255), 1)
-
-    cv2.imshow("Video", frame)
-    if 0XFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+from cascades import face_cascade
+from config import Config
 
 
+def detect_people():
+    hog = cv2.HOGDescriptor()
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+    cap = cv2.VideoCapture("../images/people_walk.mp4")
+    while True:
+        ret, img = cap.read()
+
+        if img is None:
+            print(f"Video is done. Thanks :)")
+            break
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        boxes, weights = hog.detectMultiScale(gray, winStride=(8, 8))  # FIXME it can work much better
+        print(f"Number of boxes: {len(boxes)}")
+
+        boxes = np.array([[x, y, x + w, y + w] for (x, y, w, h) in boxes])
+        for (xb, yb, wb, hb) in boxes:
+            cv2.rectangle(img, (xb, yb), (wb, hb), (0, 255, 0), 1)
+
+            person_grey = gray[yb:yb + hb, xb:xb + wb]
+            # cv2.imshow("Person", person_grey)
+            # cv2.waitKey(0)
+
+            # face_rects = face_cascade.detectMultiScale(
+            #     person_grey,
+            #     scaleFactor=Config.scaling_factor,
+            #     minNeighbors=Config.min_neighbors
+            # )
+            # print(f"Faces detected: {len(face_rects)}")
+            # for (fx, fy, fw, fh) in face_rects:
+            #     cv2.rectangle(img, (fx, fy), (fx + fw, fy + fh), (0, 0, 255), 1)
+
+        cv2.imshow('img', img)
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    detect_people()
